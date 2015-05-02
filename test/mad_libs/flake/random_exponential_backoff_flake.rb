950b47030fa32fa07c728f1b2cd943307ff49701
Danny Guinther <dannyguinther@gmail.com>
2015-05-02 16:12:11 -0400

    Refactor RandomExponentialBackoffFlake to expose rejection event

--- test/mad_libs/flake/random_exponential_backoff_flake.rb
+++ test/mad_libs/flake/random_exponential_backoff_flake.rb
@@ -8,6 +8,18 @@ module MadLibs::Flake::RandomExponentialBackoffFlake
       @time_excess = 0
     end
 
+    def decide
+      return super unless @next_call_time
+
+      call_time = Time.now.to_f
+      if call_time < @next_call_time
+        reject
+        return false
+      end
+      @time_excess += call_time - @next_call_time
+      super
+    end
+
     def on_decide(is_success)
       if is_success
         @consecutive_failures = 0
@@ -15,21 +27,14 @@ module MadLibs::Flake::RandomExponentialBackoffFlake
       else
         @consecutive_failures += 1
         term = 1 << @consecutive_failures
-        wait_time = (term + rand(term / 10 + 250)) / 1000.0
-        @next_call_time = Time.now.to_f + wait_time
+        @wait_time = (term + rand(term / 10 + 250)) / 1000.0
+        @next_call_time = Time.now.to_f + @wait_time
       end
     end
 
-    def decide
-      call_time = Time.now.to_f
-      if @next_call_time && call_time < @next_call_time
-        @call_stats[:rejected] += 1
-        return false
-      elsif @next_call_time
-        @time_excess += call_time - @next_call_time
-      end
-
-      super
+    def reject
+      @call_stats[:rejected] += 1
+      on_reject if respond_to?(:on_reject)
     end
 
     def time_stats
